26d7814654d17a37a91eb255e62d3ea3ea128b15
tamaturgo <ddst.snf20@uea.edu.br>
2021-05-31 09:14:42 -0400

    Rename files, Adding Eat sound

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ import persistence
 # Sounds
 pygame.mixer.init()
 # https://freesound.org/people/mvrasseli/sounds/553495/
-pygame.mixer.music.load("assets/sounds/553495__mvrasseli__4-4-100bpm-streets-funky-loop.wav")
+pygame.mixer.music.load("assets/sounds/game_music.wav")
 pygame.mixer.music.set_volume(1)
 pygame.mixer.music.play(-1)
 # https://freesound.org/people/harrietniamh/sounds/415079/
 death = pygame.mixer.Sound("assets/sounds/death_sound.wav")
 death.set_volume(0.5)
+# https://freesound.org/people/InspectorJ/sounds/412068/
+eat = pygame.mixer.Sound("assets/sounds/eating.wav")
+eat.set_volume(0.5)
+
 # Game Variables
 pygame.init()
 game_cycle = True
 screen = pygame.display.set_mode((800, 750))
-screen_bg = pygame.image.load("assets/images/game_screen.png")
+screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_game_screen.png")
 score = 0
 menu_controller = True
 game_controller = False
@@ -29,7 +33,7 @@ record = 0
 # -----------------------------------------------------
 
 # Score Hud
-trophy = pygame.image.load("assets/images/shiny_trophy.png")
+trophy = pygame.image.load("assets/images/lais.dib_shiny_trophy.png")
 trophy_pos = [740, 0]
 apple_catch_pos = [50, 0]
 
@@ -40,10 +44,10 @@ game_girl_20 = pygame.font.Font('assets/fonts/game_girl_classic.ttf', 20)
 game_girl_14 = pygame.font.Font('assets/fonts/game_girl_classic.ttf', 14)
 
 # Apple Controller
-point = pygame.image.load("assets/images/apple.png")
+point = pygame.image.load("assets/images/lais.dib_.png")
 point_pos_xy = [-100, -100]
 point_spawned = False
-last_key = 1  # 0 - Down, 1 - Right, 2 - Top, 3 - Left
+last_key = 1
 # Snake Controller
 pos_x = 52 + 64
 pos_y = 52 + 32
@@ -53,6 +57,7 @@ apple_value = 1
 snake_size = 1
 list_snake = []
 list_sprites = []
+snake_head = [pos_x, pos_y]
 
 
 def init_snake():
@@ -64,6 +69,7 @@ def init_snake():
     global pos_y
     global speed_x
     global speed_y
+    global last_key
     speed_x = 32
     speed_y = 0
     pos_x = 52 + 64
@@ -76,16 +82,17 @@ def init_snake():
     tail = [52+32, 52 + 32]
     list_snake.append(tail)
     list_snake.append(snake_head)
-    list_sprites = [[pygame.image.load("assets/images/snake_body.png"), 1],
-                    [pygame.image.load("assets/images/snake_body.png"), 1],
-                    [pygame.image.load("assets/images/head_right.png"), 1]]
+    last_key = 1
+    list_sprites = [[pygame.image.load("assets/images/diogeles.tamaturgo_snake_body.png"), 1],
+                    [pygame.image.load("assets/images/diogeles.tamaturgo_snake_body.png"), 1],
+                    [pygame.image.load("assets/images/lais.dib_head_right.png"), 1]]
 
 
 def draw_snake(list_position):
     index = 0
     for pos_xy in list_position:
         if not index == 0 and not index == len(list_sprites)-1:
-            list_sprites[index][0] = pygame.image.load("assets/images/snake_body.png")
+            list_sprites[index][0] = pygame.image.load("assets/images/diogeles.tamaturgo_snake_body.png")
         screen.blit(list_sprites[index][0], [pos_xy[0], pos_xy[1]])
         index += 1
 
@@ -95,24 +102,24 @@ init_snake()
 # Game Cycle
 while game_cycle:
     if menu_controller:
-        screen_bg = pygame.image.load("assets/images/menu_screen.png")
+        screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_menu_screen.png")
         mouse_pos = pygame.mouse.get_pos()
 
         if mouse_pos[0] > 158 and mouse_pos[1] > 371:
             if mouse_pos[0] < 394 and mouse_pos[1] < 451:
-                screen_bg = pygame.image.load("assets/images/play_pressed.png")
+                screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_play_pressed.png")
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         menu_controller = False
                         game_controller = True
                         pygame.mixer.music.unload()
                         # https://freesound.org/people/frankum/sounds/384468/
-                        pygame.mixer.music.load("assets/sounds/384468__frankum__vintage-elecro-pop-loop.mp3")
+                        pygame.mixer.music.load("assets/sounds/menu_music.mp3")
                         pygame.mixer.music.play(-1)
 
         if mouse_pos[0] > 158 and mouse_pos[1] > 485:
             if mouse_pos[0] < 394 and mouse_pos[1] < 554:
-                screen_bg = pygame.image.load("assets/images/credits_pressed.png")
+                screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_credits_pressed.png")
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         menu_controller = False
@@ -120,7 +127,7 @@ while game_cycle:
 
         if mouse_pos[0] > 158 and mouse_pos[1] > 610:
             if mouse_pos[0] < 394 and mouse_pos[1] < 670:
-                screen_bg = pygame.image.load("assets/images/quit_pressed.png")
+                screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_quit_pressed.png")
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         game_cycle = False
@@ -131,7 +138,7 @@ while game_cycle:
                 game_cycle = False
 
     if credits_controller:
-        screen_bg = pygame.image.load("assets/images/credits_screen.png")
+        screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_credits_screen.png")
         screen.blit(screen_bg, (0, 0))
         initial_credits_text = game_girl_32.render("CREDITS", True, (255, 255, 255))
         initial_credits_text_rect = initial_credits_text.get_rect()
@@ -142,17 +149,21 @@ while game_cycle:
         initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect.center = (250, 250)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Menu Music \n By mvrasseli", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 270)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Game Music\n By Frankum\n", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 290)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Death Sound\n By harrietniamh", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 310)
+        screen.blit(initial_credits_text, initial_credits_text_rect)
+
+        initial_credits_text = game_girl_20.render("Eating Sound\n By InspectorJ", True, (255, 255, 255))
+        initial_credits_text_rect = (120, 330)
         screen.blit(initial_credits_text, initial_credits_text_rect)
 
         # Developers
@@ -161,23 +172,22 @@ while game_cycle:
         initial_credits_text_rect.center = (250, 410)
         screen.blit(initial_credits_text, initial_credits_text_rect)
         initial_credits_text = game_girl_20.render("Dayvson Silva", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
+
         initial_credits_text_rect = (120, 430)
         screen.blit(initial_credits_text, initial_credits_text_rect)
         initial_credits_text = game_girl_20.render("Diogeles Tamaturgo", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
+
         initial_credits_text_rect = (120, 450)
         screen.blit(initial_credits_text, initial_credits_text_rect)
         initial_credits_text = game_girl_20.render("Elikson Bastos", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
+
         initial_credits_text_rect = (120, 470)
         screen.blit(initial_credits_text, initial_credits_text_rect)
         initial_credits_text = game_girl_20.render("Lais Dib", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 490)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Leandro Santiago", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 510)
         screen.blit(initial_credits_text, initial_credits_text_rect)
 
@@ -186,12 +196,12 @@ while game_cycle:
         initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect.center = (250, 600)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Game_Girl by Freaky Fonts", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 620)
         screen.blit(initial_credits_text, initial_credits_text_rect)
+
         initial_credits_text = game_girl_20.render("Fiips by pheist", True, (255, 255, 255))
-        initial_credits_text_rect = initial_credits_text.get_rect()
         initial_credits_text_rect = (120, 640)
         screen.blit(initial_credits_text, initial_credits_text_rect)
 
@@ -201,24 +211,24 @@ while game_cycle:
         screen.blit(initial_credits_text, initial_credits_text_rect)
 
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    game_cycle = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    menu_controller = True
-                    credits_controller = False
+            if event.type == pygame.QUIT:
+                game_cycle = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                menu_controller = True
+                credits_controller = False
 
     if game_controller:
-        screen_bg = pygame.image.load("assets/images/game_screen.png")
+        screen_bg = pygame.image.load("assets/images/diogeles.tamaturgo_game_screen.png")
         if not apple.apple_spawned:
             aux = random.randint(1, 10)
             if aux == 1:
-                point = pygame.image.load("assets/images/golden_apple.png")
+                point = pygame.image.load("assets/images/lais.dib_golden_apple.png")
                 apple_value = 5
             elif aux == 2:
-                point = pygame.image.load("assets/images/meat.png")
+                point = pygame.image.load("assets/images/diogeles.tamaturgo_meat.png")
                 apple_value = 2
             else:
-                point = pygame.image.load("assets/images/apple.png")
+                point = pygame.image.load("assets/images/lais.dib_.png")
                 apple_value = 1
 
             point_pos_xy = apple.spawn_apple()
@@ -265,12 +275,15 @@ while game_cycle:
             initial_score_text_rect.center = (400, 390)
             screen.blit(initial_score_text, initial_score_text_rect)
 
-            record_score_text = game_girl_32.render("Best score: " + '%03d' % persistence.consult_score(), True, (255, 255, 255))
+            record_score_text = game_girl_32.render("Best score: " + '%03d'
+                                                    % persistence.consult_score(),
+                                                    True, (255, 255, 255))
             record_score_text_rect = record_score_text.get_rect()
             record_score_text_rect.center = (400, 430)
             screen.blit(record_score_text, record_score_text_rect)
 
-            initial_score_text = game_girl_20.render("Press anything to restart", True, (255, 255, 255))
+            initial_score_text = game_girl_20.render("Press anything to restart",
+                                                     True, (255, 255, 255))
             initial_score_text_rect = initial_score_text.get_rect()
             initial_score_text_rect.center = (400, 480)
             screen.blit(initial_score_text, initial_score_text_rect)
@@ -282,7 +295,7 @@ while game_cycle:
                     game_over = False
                     score = 0
                     pygame.mixer.music.unload()
-                    pygame.mixer.music.load("assets/sounds/553495__mvrasseli__4-4-100bpm-streets-funky-loop.wav")
+                    pygame.mixer.music.load("assets/sounds/game_music.wav")
                     pygame.mixer.music.set_volume(1)
                     pygame.mixer.music.play(-1)
                     init_snake()
@@ -292,8 +305,6 @@ while game_cycle:
             if event.type == pygame.QUIT:
                 game_cycle = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    game_cycle = False
                 if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and not (last_key == 2):
                     speed_y = 32
                     speed_x = 0
@@ -340,13 +351,13 @@ while game_cycle:
 
         # Sprite Head
         if last_key == 0:
-            assets_ref = [pygame.image.load("assets/images/head_bottom.png"), 0]
+            assets_ref = [pygame.image.load("assets/images/lais.dib_head_bottom.png"), 0]
         elif last_key == 1:
-            assets_ref = [pygame.image.load("assets/images/head_right.png"), 1]
+            assets_ref = [pygame.image.load("assets/images/lais.dib_head_right.png"), 1]
         elif last_key == 2:
-            assets_ref = [pygame.image.load("assets/images/head_top.png"), 2]
+            assets_ref = [pygame.image.load("assets/images/lais.dib_head_top.png"), 2]
         else:
-            assets_ref = [pygame.image.load("assets/images/head_left.png"), 3]
+            assets_ref = [pygame.image.load("assets/images/lais.dib_head_left.png"), 3]
         list_sprites[len(list_sprites) - 1] = assets_ref
 
         # Eating the apple and increasing the snake's body
@@ -362,6 +373,7 @@ while game_cycle:
                 list_sprites.append(assets_ref)
 
             score += apple_value
+            eat.play()
             apple.apple_spawned = False
             persistence.update_score(score)
 
